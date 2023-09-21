8f41bed3500985639b89671c42f1c5c6ffcb2fb6
NDTchin <duythang19122002@gmail.com>
2023-09-22 00:30:53 +0700

    Vidu2009

---
+++ Demo/Demo/Demo 3/Demo vidu7/vidu710.c
@@ -0,0 +1,29 @@
+//
+// Created by Acer on 21/09/2023.
+//
+#include<stdio.h>
+int vidu710{
+//main(){
+    char ch;
+    printf("\nEnter a lower cased alphabet (a-z): ");
+    scanf("%c",&ch);
+    if (ch<'a'|| ch > 'z')
+        printf("\nCharacter not a lower cased alphabet");
+    else
+        switch(ch)
+        {
+            case'a':
+            case'e':
+            case'i':
+            case'o':
+            case'u':
+          printf("\nCharacter is a vowel");
+          break;
+            case'z':
+          printf("\nLast Alphabet (z) was entered");
+          break;
+          default:
+              printf("\nCharacter is a consonant");
+              break;
+        }
+}
